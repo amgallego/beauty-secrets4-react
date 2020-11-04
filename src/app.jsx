9cb0ba6cf9ee639c46b0dfb40683bb6a0b5324ba
Ana Gallego <anagallego1202@gmail.com>
2020-11-04 12:38:11 -0500

    nuevo

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+function App (){
+    return(
+        <main>
+            <div className="container"> 
+             <div className="row">
+                <div className="col"><p>inicio</p></div>
+                <div className="col"><p>desarrollo</p></div>
+                <div className="col"><p>nudo</p></div>
+                <div className="col"><p>desenlace</p></div>
+                </div>
+             </div>
+            </main>
+    )
+}
+
+export default App;
