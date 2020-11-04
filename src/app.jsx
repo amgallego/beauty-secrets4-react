818fde422999de07a10bac23500c4a6893c34ed9
Ana Gallego <anagallego1202@gmail.com>
2020-11-04 13:37:45 -0500

    borrar rama

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,7 @@ function App (){
         <main>
             <div className="container"> 
              <div className="row">
-                <div className="col"><p>inicio</p></div>
-                <div className="col"><p>desarrollo</p></div>
-                <div className="col"><p>nudo</p></div>
-                <div className="col"><p>desenlace</p></div>
-                </div>
+             </div>
              </div>
             </main>
     )
